f38706bb69950458153974d807fec3795c279162
harishkrishna5 <harishkrish09@gmail.com>
2024-12-12 23:17:40 +0530

    Create test_example.py

---
+++ tests/test_example.py
@@ -0,0 +1,10 @@
+import unittest
+from project2.main import add
+
+class TestMathOperations(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+
+if _name_ == "_main_":
+    unittest.main()
