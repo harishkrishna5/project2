e1c626f6195e55712c2fd365b5c99f35ade47acd
harishkrishna5 <harishkrish09@gmail.com>
2024-12-27 00:34:13 +0530

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,8 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import unittest
-from ..main import add
+from main import add
 
 class TestMathOperations(unittest.TestCase):
     def test_add(self):
