0aef5aabfb6c8c497707fcf6d9edc6e9cc6e511c
harishkrishna5 <harishkrish09@gmail.com>
2024-12-26 22:01:51 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  description = "The AWS region where the EC2 instance will be created"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "ami" {
+  description = "The AMI ID to launch the instance"
+  type        = string
+  default     = "ami-053b12d3152c0cc71"
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+variable "instance_name" {
+  description = "The name tag for the EC2 instance"
+  type        = string
+  default     = "My EC2 Instance"
+}
