4b2f6b4bf5913fa5d8b798cee8dc1450c7dd0188
harishkrishna5 <harishkrish09@gmail.com>
2024-12-26 23:25:52 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,6 @@ variable "region" {
 variable "ami" {
   description = "The AMI ID to launch the instance"
   type        = string
-  ami_id     = "ami-022ce6f32988af5fa"
 }
 
 variable "instance_type" {
