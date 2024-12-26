ddedf8c0edce0e33b825df1274d58626944b8f97
harishkrishna5 <harishkrish09@gmail.com>
2024-12-26 22:44:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = var.ami           # Referencing the ami variable
   instance_type = var.instance_type # Referencing the instance_type variable
+}
 
   tags = {
     Name = var.instance_name
