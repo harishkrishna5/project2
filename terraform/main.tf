fe6477b6d462e3b8c9c60624e53aed82b16be5e6
harishkrishna5 <harishkrish09@gmail.com>
2024-12-26 21:57:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,11 @@
+provider "aws" {
+  region = var.region  # Referencing the region variable
+}
 
+resource "aws_instance" "example" {
+  ami           = var.ami           # Referencing the ami variable
+  instance_type = var.instance_type # Referencing the instance_type variable
+
+  tags = {
+    Name = var.instance_name
+  }
