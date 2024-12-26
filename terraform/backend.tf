c367260881fb3f1d0b2c65cc82a30a52e051ed26
harishkrishna5 <harishkrish09@gmail.com>
2024-12-26 21:49:08 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket = "hari-demo-terraform98765"
+    key    = "terraform/state/terraform.tfstate"
+    region = "ap-south-1"
+    encrypt = true
+    dynamodb_table = "my-dynamodb-table"
+  }
+}
