ee8a79466e20eaa3d7750b54792044e91b063cdb
harishkrishna5 <harishkrish09@gmail.com>
2025-01-22 14:10:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,17 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
+  monitoring = true
   ami           = "ami-053b12d3152c0cc71"           # Referencing the ami variable
   instance_type = var.instance_type # Referencing the instance_type variable
+root_block_device {
+ encrypted     = true
+ }
+ebs_optimized = true
+metadata_options {
+      http_endpoint = "enabled"
+      http_tokens   = "required"
+  }
 
 
   tags = {
